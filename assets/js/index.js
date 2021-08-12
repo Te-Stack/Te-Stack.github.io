078a2eacb898484c15bb1a37204d17a081ef6f95
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-08-12 22:02:44 +0100

    Working

--- assets/js/index.js
+++ assets/js/index.js
@@ -76,6 +76,23 @@ class TypeWriter{
 
 }
 
+ // helper function for sending an AJAX request
+      
+ function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
 
 
 //Init on DOM Load
@@ -88,6 +105,37 @@ function init(){
     const wait = txtElement.getAttribute('data-wait')
     //Init TypeWriter
     new TypeWriter(txtElement,words,wait)
+
+    const form = document.getElementById("my-form");
+    const button = document.getElementById("my-form-button");
+    const status = document.getElementById("my-form-status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none";
+      status.innerHTML = "Thanks for reaching out !";
+      if (button.style.display === "none") {
+          status.style.display = 'block';
+      }
+      setTimeout(() => {
+          button.style = "display: block";
+          status.style.display = 'none';
+      }, 5000)
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
 
     
 }
