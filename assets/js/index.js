0ecc51537f1d74206e06701fad194035c9422d94
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2022-01-29 15:30:46 +0100

    Working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+const cursor = document.querySelector(".cursor");
+const a = document.querySelectorAll("a");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -92,6 +94,34 @@ class TypeWriter{
     };
     xhr.send(data);
   }
+
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.top = e.clientY - 10 + "px";
+    cursor.style.left = e.clientX - 10 + "px";
+  });
+  
+  a.forEach((a) => {
+    a.addEventListener("mouseover", () => {
+      cursor.style.transform = `scale(1.5)`;
+      cursor.style.opacity = 0.5;
+    });
+    a.addEventListener("mouseout", () => {
+      cursor.style.transform = `scale(1)`;
+      cursor.style.opacity = 1;
+    });
+  });
+  
+  btn.addEventListener("mouseover", () => {
+    cursor.style.transform = `scale(1.5)`;
+    cursor.style.opacity = 0.5;
+  });
+  btn.addEventListener("mouseout", () => {
+    cursor.style.transform = `scale(1)`;
+    cursor.style.opacity = 1;
+  });
+  
+
+
 
 
 
